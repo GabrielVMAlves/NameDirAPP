f4f1c93fb791fd1c02cb0ce9b890d50f81257db6
Gabriel Alves <gabriel.vma@outlook.com>
2018-04-30 17:51:00 -0300

    Alteração no modulo de alterar o nome, para funcionar em no servidor.

--- NameDirAPP/Form2.cs
+++ NameDirAPP/Form2.cs
@@ -46,7 +46,7 @@ namespace NameDirAPP
             else
             {
                 MessageBox.Show(originPath);
-                createBatch(path, newPath);
+                createBatch(path, newPath, client);
             }
         }
 
@@ -61,7 +61,7 @@ namespace NameDirAPP
             return final;
         }
 
-        public void createBatch(string o, string n)
+        public void createBatch(string o, string n, string c)
         {
             try
             {
@@ -72,10 +72,115 @@ namespace NameDirAPP
 
                 System.IO.StreamWriter sw = new StreamWriter(originPath + "\\" + "MeuArquivo.bat", false);
                 userPath = userPath.Remove(0, 2);
-                sw.WriteLine("@echo");
-                sw.WriteLine(@"CD C:\");
-                sw.WriteLine("cd "+userPath+@"\Desktop");
+
+                sw.WriteLine("@echo off");
+                sw.WriteLine("cls");
+                //entrando nos diretorios e renomeando as pastas
+                sw.WriteLine("G:");
+                sw.WriteLine("cd "+c);
                 sw.WriteLine("REN "+o+" "+n);
+                sw.WriteLine("@echo Pasta renomeada no Atendimento");
+                sw.WriteLine("I:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada na Ilustracao");
+                sw.WriteLine("J:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada na Interacao");
+                sw.WriteLine("K:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada na Midia");
+                sw.WriteLine("N:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada no Planejamento");
+                sw.WriteLine("L:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada na PreProducao");
+                sw.WriteLine("W:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada na Programacao");
+                sw.WriteLine("H:");
+                sw.WriteLine("cd " + c);
+                sw.WriteLine("REN " + o + " " + n);
+                sw.WriteLine("@echo Pasta renomeada no Projetos");
+                //criando os atalhos
+                sw.WriteLine(@"shortcut /f:'\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n  +@"\Atendimento.lnk' /a:c /t:'\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Midia$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Atendimento.lnk /a:c /t:\\CIA-SRV-FS-03\Atendimento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Ilustracao.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Interacao.lnk /a:c /t:\\CIA-SRV-FS-03\Interacao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Midia.lnk /a:c /t:\\CIA-SRV-FS-03\Ilustracao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Planejamento.lnk /a:c /t:\\CIA-SRV-FS-03\Planejamento$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\PreProducao.lnk /a:c /t:\\CIA-SRV-FS-03\PreProducao$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Projetos.lnk /a:c /t:\\CIA-SRV-FS-03\Projetos$" + c + @"\" + n + "");
+                sw.WriteLine(@"shortcut /f:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + @"\Programacao.lnk /a:c /t:\\CIA-SRV-FS-03\Programacao$" + c + @"\" + n + "");
+
                 sw.WriteLine("pause");
                 sw.Close();
 
